970a14e35a5751fa37f5700bbfe7bd0cd14c4f4d
Şerif Sezen <serifsezen23@gmail.com>
2021-01-20 23:52:45 +0300

    Add project files.

---
+++ OOP1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Id = 5;
+            product.CategoryId = 2;
+            product.ProdoctName = "monitör";
+            product.UnitPrice = 250;
+            product.UnitsInStock = 20;
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product);
+        }
+    }
+}
